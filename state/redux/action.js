b93565eb7160e0d4ead4f74f12e5ae53c6b211c7
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-06-15 09:03:30 +0530

    Update action.js

--- state/redux/action.js
+++ state/redux/action.js
@@ -17,7 +17,8 @@ const actionSlice = createSlice({
     addProduct(state, action) {
      const itemExist = state.cart.findIndex(e=>e._id === action.payload._id)
      if(itemExist >= 0 ) {
-       state.cart[itemExist].select += `<p>-><p>${action.payload.select}`
+       state.cart[itemExist].qty +=  state.cart[itemExist].qty + 1
+       state.carr[itemExist].price += state.cart[itemExist].price * state.cart[itemExist].qty
        toast.warn(`${action.payload.select} already exist`)
      }else{
        state.cart.push(action.payload)
